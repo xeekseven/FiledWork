f1cc17928c61fd8d9b501470ba5b786ae1419285
unknown <xeekseven@outlook.com>
2016-11-24 14:17:37 +0800

    First Pull

---
+++ MvpLearning/MvpLearning/Interface/UiInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvpLearning.Interface
+{
+    public interface UiInterface
+    {
+        event EventHandler UserAddEvent;
+        string UserName { get; set; }
+        string UserAge { get; set; }
+    }
+}
